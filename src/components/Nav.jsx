39c43896b32c03f8502772330684fd67056765a3
Danielle <danisteele1984@gmail.com>
2021-02-16 18:21:57 +1100

    nav update pushed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,37 +6,37 @@ function Nav(props) {
     if (props.loggedIn) {
         return (
             <nav>
-            <ul>
-                <li>
-                    <Link to="/">Scratch Pad</Link>
-                </li>
-                <li>
-                    <Link to="/profile">My Profile</Link>
-                </li>
-                <li>
-                    <Link to="/map">Map</Link>
-                </li>
-                <li>
-                    <button onClick={props.logOut}>Logout {props.name}</button>
-                </li>
-            </ul>
-        </nav>
+                <ul>
+                    <li>
+                        <Link to="/">Scratch Pad</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">My Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/map">Map</Link>
+                    </li>
+                    <li>
+                        <button onClick={props.logOut}>Logout {props.name}</button>
+                    </li>
+                </ul>
+            </nav>
         ) 
     } else {
         return (
             <nav>
-            <ul>
-                <li>
-                    <Link to="/">Scratch Pad</Link>
-                </li>
-                <li>
-                    <Link to="/register">Sign up</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
-        </nav>
+                <ul>
+                    <li>
+                        <Link to="/">Scratch Pad</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Sign up</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
